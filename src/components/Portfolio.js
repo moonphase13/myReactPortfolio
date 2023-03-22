a4c5065ea992883f405051fa7ec90e3fda33a2a9
Michael <fraynemichael300@gmail.com>
2023-03-21 18:48:37 -0600

    got about me and footer to display

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,21 @@
 import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 function Portfolio () {
 
   return (
-    <header className='header'>
-      <Header />
-    </header>
+    <div>
+      <header className='header'>
+        <Header />
+      </header>
+      <main className='main'>
+        <Main />
+      </main>
+      <footer className='footer'>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
