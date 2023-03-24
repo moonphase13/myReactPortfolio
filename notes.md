da18e196429424784573733cb9a1c06da1805b93
Michael <fraynemichael300@gmail.com>
2023-03-23 21:15:53 -0600

    added form confirmation using fragments and useState

--- notes.md
+++ notes.md
@@ -4,7 +4,7 @@
 - [x] **`TODO:`** add shadows to cards *NOTE:* scrapped this idea
 - [x] **`TODO:`** `max-width` for nav items set to `min-content`
 - [x] **`TODO:`** change main headers to `lg:justify-start` and `justify-center`
-- [ ] **`TODO:`** adjust spacing on contactMe section
+- [x] **`TODO:`** adjust spacing on contactMe section
 - [ ] **`TODO:`** style submit button on contactMe section
 - [ ] **`TODO:`** add logic that when 2 of the fields are filled in but one is not filled in or not filled in correctly it displays in the DOM
 - [ ] **`TODO:`** add minor transitions/animations
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,47 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function ContactMe() {
-  const [input, setInput] = useState('');
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    const errors = Object.values(formErrors);
+    setFormIsValid(errors.every(error => error === ''));
+  }, [formErrors]);
 
   const handleInputChange = (event) => {
-    setInput(event.target.value);
+    const { name, value } = event.target;
+    let error = '';
+
+    switch (name) {
+      case "name":
+        error = value.trim() === '' ? 'Name is required' : '';
+        break;
+      case "email":
+        error = value.trim() === '' ? 'Email is required' : !value.match(/^\S+@\S+\.\S+$/) ? 'Email is invalid' : '';
+        break;
+      case "message":
+        error = value.trim() === '' ? 'Message is required' : '';
+        break;
+      default:
+        break;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setFormErrors({
+      ...formErrors,
+      [name]: error,
+    });
   };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    setInput('');
+    if (formIsValid) {
+      // DO SOMETHING WITH THE FORM DATA
+      setFormData({
+        name: '',
+        email: '',
+        message: '',
+      });
+      setFormErrors({
+        name: '',
+        email: '',
+        message: '',
+      });
+      setFormIsValid(false);
+    }
   };
+
+  const inputFields = [
+    { name: "name", type: "text", label: "Name" },
+    { name: "email", type: "email", label: "Email" },
+    { name: "message", type: "textarea", label: "Message" },
+  ];
 
   return (
     <div className="flex flex-col justify-center items-center z-0">
       <h2 className="text-4xl lg:text-5xl font-bold mb-5 self-center lg:self-start lg:ml-10">Contact Me</h2>
       <form className="flex flex-col self-center lg:self-start lg:ml-10 lg:w-1/2 md:w-7/12" onSubmit={handleFormSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          value={input}
-          onChange={handleInputChange}
-          className="border-bk border-2 rounded-md p-2 mb-5 mt-2"
-          autocomplete='off'
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={input}
-          onChange={handleInputChange}
-          className="border-bk border-2 rounded-md p-2 mb-5 mt-2"
-          autocomplete='off'
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          name="message"
-          value={input}
-          onChange={handleInputChange}
-          className="border-bk border-2 rounded-md p-2 mb-5 mt-2"
-        />
-        <button type="submit" className="border-bk border-2 rounded-md p-2">
+        {inputFields.map(({ name, type, label }) => (
+          <React.Fragment key={name}>
+            <label htmlFor={name}>{label}</label>
+            <input
+              type={type}
+              name={name}
+              value={formData[name]}
+              onChange={handleInputChange}
+              className={`border-bk border-2 rounded-md p-2 mb-5 mt-2 bg-transparent ${
+                formErrors[name] && "border-red-500"
+              }`}
+              autoComplete="off"
+              required={name === "name" || name === "email" || name === "message"}
+            />
+            {formErrors[name] && <span className="text-red-500 text-sm mb-5 ml-2">{formErrors[name]}</span>}
+          </React.Fragment>
+        ))}
+        <button type="submit" className="border-bk border-2 rounded-md p-2 w-fit self-center lg:self-start" disabled={!formIsValid}>
           Submit
         </button>
       </form>
@@ -49,4 +102,4 @@ function ContactMe() {
   );
 }
 
-export default ContactMe;
+export default ContactForm;
